7b8a06489c4d37cf0e8df6037ee657dfdb250531
Sajjad Saju <148693286+sajjadsajux@users.noreply.github.com>
2025-05-20 03:29:57 +0600

    Firebase Setup Done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCOF8DAvv155DkgNm3x8RUgMzEZ3hjnKw",
-  authDomain: "planttick.firebaseapp.com",
-  projectId: "planttick",
-  storageBucket: "planttick.firebasestorage.app",
-  messagingSenderId: "407652744223",
-  appId: "1:407652744223:web:8a1be902c4225f036ae92a",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
