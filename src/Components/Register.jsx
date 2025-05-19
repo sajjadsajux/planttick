43c2e5e4d7d7bae15da2ecfcfe9074723e5da679
Sajjad Saju <148693286+sajjadsajux@users.noreply.github.com>
2025-05-19 21:58:10 +0600

    Added Register Page

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h2>Register</h2>
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100 mx-auto lg:mt-20">
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+        <form className="space-y-6" onSubmit={handleRegister}>
+          {/* name */}
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-400">Name</label>
+            <input type="text" name="name" placeholder="Name" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 " />
+          </div>
+          {/* email */}
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-400">Email</label>
+            <input type="email" name="email" placeholder="Email" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 " />
+          </div>
+          {/* photourl */}
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-400">PhotoURL</label>
+            <input type="url" name="photo" placeholder="PhotoURL" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 " />
+          </div>
+          {/* Pass */}
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-400">Password</label>
+            <input type="password" name="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 " />
+          </div>
+          <button type="submit" className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400">
+            Register
+          </button>
+        </form>
+        <p className="text-xs text-center sm:px-6 text-gray-400">
+          Already have an account?{" "}
+          <Link to="/login" className="underline text-gray-100">
+            Login Here
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
