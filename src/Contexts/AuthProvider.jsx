5db1380ff93fc3a3300fc11d0a5031a918ffd40a
Sajjad Saju <148693286+sajjadsajux@users.noreply.github.com>
2025-06-25 08:47:43 +0600

    firebase key Rotated

--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-import { auth } from "../firebase.init";
+import { auth } from "../firebase.initConfig";
 
 const provider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
